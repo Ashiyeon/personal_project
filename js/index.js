4c2aa025ee23e5d2c619f6f9d3736165ffeb4a18
Ashiyeon <oopschristian@gmail.com>
2023-07-04 01:20:34 +0800

    js 更新

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@
 
 
 let scrollDistance = 0;
-const targetScroll = 200;
+const targetScroll = 400;
 const targetElement = document.querySelector('.top-event');
 
 // 事件監聽函數
@@ -55,90 +55,54 @@ window.addEventListener('wheel', onScroll, { passive: false });
 window.addEventListener('scroll', checkScrollPosition);
 
 
+//新聞輪播
 
-
-
-
-
-
-
-
-// 新聞輪播
-
-// 選取輪播元素
 const carousel = document.querySelector(".carousel");
-
-// 選取第一個新聞輪播圖片
 firstImg = document.querySelectorAll(".news-slider")[0];
-
-// 選取箭頭圖示元素
 arrowIcons = document.querySelectorAll(".news i");
 
 let isDragStart = false, prevPageX, prevScrollLeft, positionDiff;
-
-// 計算第一個新聞輪播圖片的寬度（包含間距）
 let firstImgWidth = firstImg.clientWidth + 16;
 
-// 監聽箭頭圖示的點擊事件
 arrowIcons.forEach(icon =>{
     icon.addEventListener("click", () =>{
-        // 根據點擊的箭頭方向，調整輪播元素的捲動位置
         carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
     });
 });
 
-// 自動滑動輪播元素
 const autoSlide = () => {
-    // 計算新的位置差
     positionDiff = Math.abs(positionDiff);
-
-    // 計算新的第一個新聞輪播圖片的寬度
     let firstImgWidth = firstImg.clientWidth + 16;
-
-    // 計算捲動的值差
     let valDifference = firstImgWidth - positionDiff;
 
-    if (carousel.scrollLeft > prevScrollLeft) {
-        // 向右滑動
+    if(carousel.scrollLeft > prevScrollLeft){
         return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
     }
-    // 向左滑動
     carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
 }
 
 const dragStart = (e) => {
-    // 開始拖曳事件
+    //按下事件
     isDragStart = true;
-
-    // 記錄開始拖曳時的頁面X座標
     prevPageX = e.pageX;
-
-    // 記錄開始拖曳時的捲動位置
     prevScrollLeft = carousel.scrollLeft; 
 }
 
 const dragging = (e) =>{
-    if (!isDragStart) return;
-
-    e.preventDefault();
-
-    // 加上拖曳中的樣式
+    if(!isDragStart) return;
+    e.preventDefault(); //關閉預設
     carousel.classList.add("dragging");
-
-    // 計算新的位置差
-    positionDiff = e.pageX - prevPageX;
-
-    // 設定新的捲動位置
+    positionDiff = e.pageX -prevPageX
     carousel.scrollLeft = prevScrollLeft - positionDiff;
 }
 
 const dragStop = () => {
-    // 停止拖曳事件
     isDragStart = false;
-
-    // 移除拖曳中的樣式
     carousel.classList.remove("dragging");
-
-    // 執行自動滑動
     autoSlide();
 }
+
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mousemove", dragging);
